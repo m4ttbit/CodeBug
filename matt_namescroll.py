d1f4760ad1320ebd4f531188eba0d6b43187d64e
Matt <blucircuit@outlook.com>
2016-10-18 16:01:23 -0400

    Update with new code

--- matt_namescroll.py
+++ matt_namescroll.py
@@ -9,6 +9,7 @@ from codebug_tether import (IO_DIGITAL_OUTPUT,
                             IO_ANALOG_INPUT)
 from codebug_tether.sprites import Sprite
 import time
+from codebug_tether.sprites import StringSprite
 
 codebug = codebug_tether.CodeBug()
 codebug.set_leg_io(0, IO_DIGITAL_INPUT);
@@ -27,16 +28,24 @@ def build_sprite(rows):
   return s
 
 
+# interrupts -- and therefore sleep -- not supported in Python
 while True:
-  if codebug.get_input('A') == 1:
-    for count in range(5):
-      codebug.scroll_sprite(build_sprite([0b10001,0b10001,0b10101,0b11011,0b10001]), 75/1000, 'L')
-      codebug.scroll_sprite(build_sprite([0b01010,0b01010,0b01110,0b01010,0b00100]), 75/1000, 'L')
-      codebug.scroll_sprite(build_sprite([0b00100,0b00100,0b00100,0b00100,0b11111]), 75/1000, 'L')
-      codebug.scroll_sprite(build_sprite([0b00100,0b00100,0b00100,0b00100,0b11111]), 75/1000, 'L')
-      time.sleep(1000/1000);
-  elif codebug.get_input('B') == 1:
-    codebug.draw_sprite(0, 0, build_sprite([0b01110,0b10001,0b00000,0b01010,0b01010]));
-    time.sleep(7000/1000);
-  else:
-    # interrupts -- and therefore sleep -- not supported in Python
+  codebug.draw_sprite(0, 0, build_sprite([0b10001,0b00000,0b00000,0b00000,0b10001]));
+  time.sleep(100/1000);
+  codebug.draw_sprite(0, 0, build_sprite([0b10001,0b01010,0b00000,0b01010,0b10001]));
+  time.sleep(100/1000);
+  codebug.draw_sprite(0, 0, build_sprite([0b00000,0b01010,0b00100,0b01010,0b00000]));
+  time.sleep(100/1000);
+  codebug.draw_sprite(0, 0, build_sprite([0b00000,0b00000,0b00100,0b00000,0b00000]));
+  time.sleep(100/1000);
+  for count in range(5):
+    codebug.draw_sprite(0, 0, build_sprite([0b00000,0b01110,0b01010,0b01110,0b00000]));
+    time.sleep(100/1000);
+    codebug.draw_sprite(0, 0, build_sprite([0b11111,0b10001,0b10101,0b10001,0b11111]));
+    time.sleep(100/1000);
+  codebug.draw_sprite(0, 0, build_sprite([0b00000,0b01110,0b01010,0b01110,0b00000]));
+  time.sleep(100/1000);
+  codebug.draw_sprite(0, 0, build_sprite([0b11111,0b10001,0b10001,0b10001,0b11111]));
+  codebug.draw_sprite(0, 0, build_sprite([0b00000,0b00000,0b00000,0b00000,0b00000]));
+  time.sleep(2000/1000);
+  codebug.scroll_sprite(StringSprite('MATT ARNOLD', 'R'), 100/1000, 'L')
